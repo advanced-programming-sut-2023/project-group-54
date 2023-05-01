939d9ca4399a7bcfefcc0411f41a9172fa973df2
mfamin <mrmfamin835@yahoo.com>
2023-05-01 17:29:44 +0330

    some change and debugging for sign up and login

--- src/main/java/model/Buildings/Building.java
+++ src/main/java/model/Buildings/Building.java
@@ -1,12 +1,14 @@
 package model.Buildings;
 
+import model.Map;
+
 import java.util.HashMap;
 
 public class Building {
     private static HashMap<Building, Integer> buildings;
     private BuildingType buildingType;
     private int hp;
-
+    private Map[][] mapPositions = new Map[buildingType.getWidth()][buildingType.getLength()];
     public Building(BuildingType buildingType, int maxHp) {
 
     }
