75273c9135b4e2748f85de5654c9a671d45e8868
amirahsh <amirhoseinshahidi20041382@gmail.com>
2023-05-13 16:33:40 +0330

    Arian was here

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import view.MainMenu;
 
 public class Main {
     public static void main(String[] args) {
+        //Arian was here
         MainMenu menu = new MainMenu();
         menu.run();
     }
