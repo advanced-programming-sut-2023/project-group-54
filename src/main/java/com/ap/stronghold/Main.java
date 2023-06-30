9c58ddd20a30de4b295a87c48743633d813cc487
mobinnbagheriann <morobot2003@gmail.com>
2023-06-30 18:08:03 +0330

    trade Menu added

--- src/main/java/com/ap/stronghold/Main.java
+++ src/main/java/com/ap/stronghold/Main.java
@@ -3,8 +3,7 @@ package com.ap.stronghold;
 import com.ap.stronghold.model.User;
 import com.ap.stronghold.view.MainMenu;
 
-public class
-Main {
+public class Main {
     public static void main(String[] args) throws Exception {
 
         User.loadUser();
---
+++ src/main/java/com/ap/stronghold/view/testMenu.java
@@ -0,0 +1,34 @@
+package com.ap.stronghold.view;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class testMenu extends Application {
+
+
+
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+return;
+
+    }
+}
